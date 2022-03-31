171c90efb3fa031bfb42330a9a239208b8ddbda1
keknadr <pslups23@gmail.com>
2022-03-31 12:52:06 +0300

    Added spinner and error gif

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,7 @@
+const Spinner = () => {
+    return ( 
+        <svg xmlns="http://www.w3.org/2000/svg" style={{margin: 'auto', background: '#fff', display: 'block'}} width="130" height="130" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><path d="M10 50a40 40 0 0 0 80 0 40 42.6 0 0 1-80 0" fill="#9f0013"><animateTransform attributeName="transform" type="rotate" dur="1s" repeatCount="indefinite" keyTimes="0;1" values="0 50 51.3;360 50 51.3"/></path></svg>
+    )
+}
+
+export default Spinner;
